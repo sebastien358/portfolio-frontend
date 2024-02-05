a73bd682d7113d44723121c110a5bbfb81007da0
Sébastien Petit <sebastienpetit27330@gmail.com>
2024-02-05 03:44:49 +0100

    update header

--- src/features/portfolio/home/views/Home.vue
+++ src/features/portfolio/home/views/Home.vue
@@ -15,7 +15,7 @@
           </div>
 
           <div class="mt-4">
-            <h3 class="text-center text-uppercase">Développeuse web</h3>
+            <h3 class="text-center text-uppercase">Développeur web</h3>
             <h4 class="text-center">Portfolio</h4>
           </div>
           <a href="mailto:sebastienpetit27330@gmail.com" class="button">Contactez-moi !</a>
