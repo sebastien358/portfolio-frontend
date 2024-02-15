5d02deeffada1962e8e68501eb71af59e9f7d060
Sébastien Petit <sebastienpetit27330@gmail.com>
2024-02-15 18:36:24 +0100

    update css cv responsive

--- src/features/portfolio/cv/views/Cv.vue
+++ src/features/portfolio/cv/views/Cv.vue
@@ -6,9 +6,10 @@
           <div class="left-column">
             <img class="portait" src="@/assets/images/sebastien.jpg" />
             <div class="section">
-              <p>
-                <i class="icon fab fa-linkedin text-darkblue"></i> pierre-gomba
-              </p>
+              <a href="https://www.linkedin.com/in/s%C3%A9bastien-petit-1874141b9/" target="_blank" class="text-white">
+                <font-awesome-icon icon="fa-brands fa-linkedin" class="text-white" />
+                Sébastien Petit
+              </a>
             </div>
             <div class="section">
               <h2>À PROPOS</h2>
@@ -22,52 +23,59 @@
             <div class="section">
               <h2>COMPÉTENCES</h2>
               <ul class="skills">
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>Angular &#124; Typescript</strong></li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>Bootstrap</strong></li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>HTML5 &#124; CSS3 &#124; SASS</strong></li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>Javascript</strong></li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>jQuery</strong></li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> <strong>npm &#124; Webpack</strong></li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> HTML5 &#124; CSS3 &#124; SASS</li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> Bootstrap</li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> JavaScript &#124;</li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> Vue.js &#124; VueX</li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> Pinia</li>
                 <li><i class="icon fas fa-check-circle text-darkblue"></i> PHP</li>
-                <li><i class="icon fas fa-check-circle text-darkblue"></i> Zend Framework</li>
+                <li><i class="icon fas fa-check-circle text-darkblue"></i> Symfony</li>
                 <li><i class="icon fas fa-check-circle text-darkblue"></i> MySQL</li>
                 <li><i class="icon fas fa-check-circle text-darkblue"></i> Git &#124; Github</li>
               </ul>
             </div>
             <div class="section">
               <h2>Langues</h2>
-              <p>
-                Français, langue maternelle
-                <br>
-                Anglais, compétence professionnelle
-              </p>
+              <p>Français, langue maternelle</p>
             </div>
             <div class="section">
               <h2>Centres d'intérêt</h2>
               <p>
-                Jeux vidéo, jouer et développer
+                Jeux vidéo
                 <br>
-                Musique, écoute et composition
+                Musique
                 <br>
                 Art en général
                 <br>
                 Sport
                 <br>
-                Informatique en général
+                Informatique
               </p>
             </div>
           </div>
           <div class="right-column bg-white">
             <div class="header">
-              <h1>Pierre <span class="text-blue text-uppercase">Gomba</span></h1>
-              <p>Freelance Front-end Developer</p>
+              <h1>Sébastien <span class="text-blue text-uppercase">Petit</span></h1>
+              <p>Développeur Full-Stack junior</p>
               <ul class="infos">
-                <li><i class="icon fas fa-at text-blue"></i> <a href="mailto:contact@pgomba.com">contact@pgomba.com</a></li>
-                <li><i class="icon fas fa-phone text-blue"></i> 04 75 53 80 50</li>
-                <li><i class="icon fas fa-map-marker-alt text-blue"></i> Boulevard de la Constitution 31, 4020 Liège</li>
+                <li><i class="icon fas fa-at text-blue"></i> <a href="mailto:contact@pgomba.com">sebastienpetit27330@gmail.com</a></li>
+                <li><i class="icon fas fa-phone text-blue"></i> 06 04 42 34 56</li>
+                <li><i class="icon fas fa-map-marker-alt text-blue"></i> 11 Rue du Val Ferrant, 27330 Bois-Anzeray</li>
               </ul>
             </div>
             <div class="content">
+              <div class="section">
+                <h2>Études <br><span class="text-blue">& formations</span></h2>
+                <p>
+                  <strong>2023 <i class="fas fa-long-arrow-alt-right"></i> 2024</strong>
+                  <br>
+                  Autodidacte en développement web de niveau bac+2, je suis à la recherche d'un premier
+                  emploi dans ce milieu qui me passionne. Impliqué et motivé, je souhaite acquérir de
+                  l'expérience et des responsabilités afin de m'épanouir dans ma carrière. Les cours particuliers
+                  que j'ai suivis sur la plateforme Superprof.fr m'ont permis de monter en compétences et de
+                  me spécialiser avec Symfony, Vue.js
+                </p>
+              </div>
               <div class="section">
                 <h2>Expériences <br><span class="text-blue">professionelles</span></h2>
                 <p>
@@ -76,65 +84,19 @@
                   Fullstack Developer à temps plein chez <em>Webadev SPRL</em>
                 </p>
                 <ul class="experience-list">
-                  <li>Réalisations de sites web, d’e-shops, d’interfaces et d’applications web sous Angular et Bootstrap</li>
-                  <li>Intégration de templates Photoshop, Illustrator, Sketch, Figma</li>
+                  <li>Réalisations de sites web, d’applications web sous Vue.js et Bootstrap, CSS3, SASS</li>
+                  <li>Intégration Figma</li>
                   <li>Animations CSS / JS</li>
                   <li>Responsive design</li>
                   <li>UI / UX Design</li>
-                  <li>Projets sous npm et Webpack</li>
-                  <li>Collaboration avec d’autres studios graphique (Studio Debie, SOL,…)</li>
-                  <li>Optimisation des performances</li>
-                  <li>Développement de templates et de modules réutilisables</li>
-                  <li>Projets en équipe, utilisation quotidienne de SVN, Git et Github</li>
+                  <li>Utilisation quotidienne de Git et Github</li>
                 </ul>
               </div>
               <div class="section">
-                <p>
-                  <strong>2021</strong>
-                  <br>
-                  Freelance en activité
-                </p>
-                <ul class="experience-list">
-                  <li>Freelance Front-end Developer</li>
-                  <li>Unity Developer / Sound Designer</li>
-                </ul>
-              </div>
-              <div class="section">
-                <h2>Études <br><span class="text-blue">& formations</span></h2>
-                <p>
-                  <strong>2015 <i class="fas fa-long-arrow-alt-right"></i> 2019</strong>
-                  <br>
-                  <em>Bachelier en Informatique de Gestion</em>, Diplômé, Institut Saint Laurent
-                </p>
-                <p>
-                  <strong>2015</strong>
-                  <br>
-                  <em>STE-Formations Informatiques</em>, Formation qualifiante de Web Developer
-                </p>
-                <p>
-                  <strong>2013 <i class="fas fa-long-arrow-alt-right"></i> 2014</strong>
-                  <br>
-                  <em>Bachelier en Sciences humaines</em>, Haute École de Liège
-                </p>
-                <p>
-                  <strong>2009 <i class="fas fa-long-arrow-alt-right"></i> 2013</strong>
-                  <br>
-                  <em>Bachelier en Psychologie</em>, Université de Liège
-                </p>
-                <p>
-                  <strong>2002 <i class="fas fa-long-arrow-alt-right"></i> 2009</strong>
-                  <br>
-                  <em>CESS</em>, Institut Notre-Dame de Jupille
-                </p>
-              </div>
-              <div class="section">
-                <h2>Autres <br><span class="text-blue">expériences</span></h2>
+                <h2>Autres</h2>
                 <p>
                   Permis B, possession d’une voiture
                   <br>
-                  Animateur Scout pendant 6 ans
-                  <br>
-                  Brevet d’animateur de Centre de Vacances
                 </p>
               </div>
             </div>
@@ -155,74 +117,99 @@ import BaseTemplate from "@/BaseTemplate.vue";
 .bg-page {
   background: var(--bg-page);
   padding: 40px 20px;
+  @include m.lg {
+    padding: 30px 20px;
+  }
 }
 
-
-a{
+a {
   text-decoration: none;
   color: #4472C4;
 }
 
-a:hover{
+a:hover {
   text-decoration: underline;
 }
 
-p{
+p {
   margin: 0 0 1rem;
 }
 
-h1{
+h1 {
   margin: 0 0 1rem;
   font-size: 2.5rem;
   margin-bottom: .5rem;
+  @include m.lg {
+    font-size: 2.1rem;
+  }
+  @include m.sm {
+    font-size: 1.7rem;
+  }
 }
 
-h2{
+h2 {
   margin: 0 0 1rem;
   letter-spacing: 1px;
   text-transform: uppercase;
+  font-size: 24px;
+  @include m.lg {
+    font-size: 20px
+  }
 }
 
-.text-blue{
+.text-blue {
   color: #4472C4;
 }
 
-.text-darkblue{
+.text-darkblue {
   color: #002060;
 }
 
-.text-uppercase{
+.text-uppercase {
   text-transform: uppercase;
 }
 
-.icon{
+.icon {
   margin-right: .5rem;
 }
 
-.cv-container{
+.cv-container {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-areas: "left-column right-column right-column";
+  grid-template-columns: 400px 1fr;
+  grid-template-areas: "left-column right-column";
   width: 1200px;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   @include m.xxl {
     width: 100%;
   }
+  @include m.lg {
+    display: initial;
+    width: 100%;
+  }
 }
 
-.section{
+.section {
   margin-bottom: 1.5rem;
 }
 
 /* LEFT COLUMN */
-.left-column{
+.left-column {
   grid-area: left-column;
   padding: 50px;
   background-color: #4472C4;
   color: white;
+  @include m.lg {
+    padding: 20px;
+  }
+  p {
+    font-size: 16px;
+    @include m.lg {
+      font-size: 15px;
+    }
+  }
 }
 
-.portait{
+.portait {
   border-radius: 50%;
   width: 150px;
   height: 150px;
@@ -231,47 +218,88 @@ h2{
   margin-bottom: 50px;
 }
 
-.skills{
+.skills {
   list-style-type: none;
   padding: 0;
   font-size: 1.1rem;
   letter-spacing: 1px;
   margin: 0 0 1rem;
+  @include m.lg {
+    font-size: 1rem;
+  }
 }
 
 /* RIGHT COLUMN */
-.right-column{
+
+.right-column {
   grid-area: right-column;
   display: grid;
   grid-template-rows: 250px 1fr;
-  grid-template-areas:
-    "header"
-    "content";
+  grid-template-areas: "header" "content";
+  @include m.lg {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
 }
 
 /* HEADER */
-.header{
+.header {
   grid-area: header;
   padding: 50px;
   background-color: #F2F2F2;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  @include m.lg {
+    padding: 20px;
+  }
+  p {
+    font-size: 16px;
+  }
 }
 
-.infos{
+.infos {
   columns: 2;
   list-style-type: none;
   padding: 0;
+  @include m.lg {
+    display: flex;
+    flex-direction: column;
+  }
+  li {
+    font-size: 16px;
+  }
 }
 
 /* CONTENT */
-.content{
+
+.content {
   grid-area: content;
   padding: 50px;
+  @include m.lg {
+    padding: 20px;
+  }
+  .section {
+    p {
+      @include m.lg {
+        font-size: 16px;
+      }
+      @include m.sm {
+        font-size: 15px;
+      }
+    }
+  }
 }
 
-.experience-list{
+.experience-list {
   list-style-type: circle;
+  li {
+    @include m.lg {
+      font-size: 16px;
+    }
+    @include m.sm {
+      font-size: 15px;
+    }
+  }
 }
 </style>
